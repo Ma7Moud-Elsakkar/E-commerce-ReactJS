d7c5820bb65e65f7138eaa2ac16da345ed1ae549
Ma7Moud-Elsakkar <mahmoudelsakkar12@gmail.com>
2026-03-28 14:41:35 +0200

    chenge

--- src/Components/header/HeroSlider.jsx
+++ src/Components/header/HeroSlider.jsx
@@ -1,4 +1,4 @@
-// Import Swiper React components
+import React, { Component } from 'react' ;
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
