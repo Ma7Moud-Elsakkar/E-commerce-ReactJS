5f0f72aac24625ab2cf6d6f0bb3eb426add14186
Ma7Moud-Elsakkar <mahmoudelsakkar12@gmail.com>
2026-03-28 15:20:43 +0200

    8

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import CartProvider from './Components/header/context/CartContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/'>
+    <HashRouter>
       <CartProvider>
         <App />
       </CartProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
